ebc6d6a3292f620e1153697ed791ba6bbdc9f680
Asbjørn Djupdal <djupdal@ntnu.no>
2020-01-15 08:58:06 +0100

    Changed incorrect license information in source files

--- mcu/main/jtag_lowlevel.h
+++ mcu/main/jtag_lowlevel.h
@@ -4,19 +4,6 @@
  *
  *  Copyright 2018-2019 Asbjørn Djupdal, NTNU
  *
- *  Lynsyn is free software: you can redistribute it and/or modify
- *  it under the terms of the GNU General Public License as published by
- *  the Free Software Foundation, either version 3 of the License, or
- *  (at your option) any later version.
- *
- *  Lynsyn is distributed in the hope that it will be useful,
- *  but WITHOUT ANY WARRANTY; without even the implied warranty of
- *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *  GNU General Public License for more details.
- *
- *  You should have received a copy of the GNU General Public License
- *  along with Lynsyn.  If not, see <http://www.gnu.org/licenses/>.
- *
  *****************************************************************************/
 
 #ifndef JTAG_LOWLEVEL_H
